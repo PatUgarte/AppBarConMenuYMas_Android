6225cb8b5496d02bbac2bc9e65a68e7adbc83956
Pat <patriciomugarte@gmail.com>
2019-06-30 01:21:54 -0300

    Algunos arreglos menores y agregado de comentarios para detallar cada parte.

--- app/src/main/java/digitalhouse/com/appbar/PerfilActivity.java
+++ app/src/main/java/digitalhouse/com/appbar/PerfilActivity.java
@@ -14,17 +14,26 @@ public class PerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+        //TRAEMOS LA Toolbar CON EL id CORRESPONDIENTE AL include QUE HICIMOS EN EL activity_perfil.xml
         Toolbar unToolBar = findViewById(R.id.toolbar_activity_perfil);
+        //EL include LA AÑADE VISUALMENTE, HACEMOS EL setSupportActionBar() PARA CONFIGURARLE FUNCIONALIDADES COMO EL MENÚ
         setSupportActionBar(unToolBar);
 
-        //PARA AGREGARLE EL BOTON BACK
+        //PARA COFIGURARLE A LA Toolbar LA FLECHA DE REGRESO A LA PRIMER PANTALLA NECESITAMOS TRAERNOS LA ActionBar QUE SETEAMOS ARRIBA
         ActionBar unActionBar = getSupportActionBar();
+        //CON ESTA ActionBar PODEMOS AÑADIRLE LA FLECHA DE RETORNO CON EL SIGUIENTE MÉTODO:
         unActionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
+    /*
+    * CON SÓLO HACER EL setDisplayHomeAsUpEnabled() YA ALCANZA PARA QUE LA FLECHA VUELVA A LA PANTALLA ANTERIOR (HABIENDO SETEADO EL parentActivityName EN EL manifest
+    *
+    * SI QUEREMOS CONFIGURAR NUESTRO PROPIO COMPORTAMIENTO PODEMOS SOBREESCRIBIR EL MÉTODO onSupportNavigateUp() DE LA Activity
+    */
     @Override
     public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+        Toast.makeText(this, "Este Toast se hizo antes de volver a la MainActivity", Toast.LENGTH_SHORT).show();
+        return super.onSupportNavigateUp();
     }
 }
